065b8777ababa1c9074a4aebebfb331d111eac2e
riszkymf <riszkymfahreza@gmail.com>
2019-03-12 14:30:12 +0700

    moving files and adding init

--- omnibus/simple_test.py
+++ omnibus/simple_test.py
@@ -1,19 +1,36 @@
-from omnibus.libs.content_parser import *
+# from omnibus.libs.content_parser import *
+# import os
+# import requests
+# from omnibus.libs.binding import Context
+# from omnibus.libs import util
+
+
+
+# base_url = 'http://127.0.0.1:6968/'
+# test_file = ['omnibus/test_copy.yml']
+# test_structure = list()
+# paths = list()
+# for i in test_file:
+#     test_structure.append(util.load_yaml(i))
+#     paths.append(os.path.dirname(i))
+
+# for t,p,f in zip(test_structure,paths,test_file):
+#     var = {"mock_zone": 'test.com'}
+#     tests = parse_file(test_structure,test_file,vars=var,global_url=base_url)
+#     res = run_testsets(tests)
+
+
+
+from omnibus.omnibus.libs.content_parser import *
 import os
 import requests
-from omnibus.libs.binding import Context
-from omnibus.libs import util
-
-
+from omnibus.omnibus.libs.binding import Context
+from omnibus.omnibus.libs import util
 base_url = 'http://127.0.0.1:6968/'
-test_file = ['omnibus/test_copy.yml']
-test_structure = list()
-paths = list()
-for i in test_file:
-    test_structure.append(util.load_yaml(i))
-    paths.append(os.path.dirname(i))
+test_file = ['omnibus/omnibus/test_copy.yml']
+test_file = test_file[0]
+test_structure = util.load_yaml(test_file)
+var = {"mock_zone": 'test.com'}
+tests = parse_file(test_structure,test_file,vars=var,global_url=base_url)
+result = run_testsets(tests)
 
-for t,p,f in zip(test_structure,paths,test_file):
-    var = {"mock_zone": 'test.com'}
-    tests = parse_file(t,f,p,vars=var,global_url=base_url)
-    res = run_testsets(tests)
